abde41e920bffca78ad3e0d53f463f1c67b37809
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2022-12-09 14:38:32 +0100

    Testing api pinia typescript

--- pages/panel/moje-konto/moje-srodki.vue
+++ pages/panel/moje-konto/moje-srodki.vue
@@ -7,18 +7,32 @@
           <h2 class="title-hero mb-32">Moje środki</h2>
         </div>
       </div>
-      <div class="w-full h-8">
-
+      <div class="">
+{{quiz.fastTwo}}
       </div>
-{{test.tests.data.personal}}
+      <div class="mt-20">
+        <div
+        v-for="response in test.getTestl"
+        :key="response"
+        class="grid grid-cols-4 gap-8"
+      >
+      <div v-for="test in response" :key="test">
+      </div>
+</div>
+      </div>
+      {{test.test.name}}
     </NuxtLayout>
   </div>
 </template>
 
 <script setup lang="ts">
 import { testStore } from "@/store/testStore";
+import {Quiz} from "@/store/useQuizzes";
 
-const test = testStore();
-test.getTestl();
-console.log(test)
+const quiz = Quiz()
+quiz.getFastTwo()
+
+
+const test = testStore()
+test.getTestl()
 </script>
--- store/testStore.ts
+++ store/testStore.ts
@@ -3,21 +3,35 @@ import { axiosInstance } from '@/axios.config'
 import { UserSettingsPersonal, UserSettingsFinancial, UserSettingsCompanyAddress, UserSettingsCompany } from '@/api/types'
 
 interface Test {
+    personal:{
+    email: string
+    name: string
+    phone: string
+    surname: string
+    }
+}
+
+interface Personal{
     email: string
     name: string
     phone: string
     surname: string
 }
 
+interface State {
+   test: Personal
+}
+
+
 export const testStore = defineStore('test', {
     state: () => ({
-        tests: []
+        test:{} as Personal
     }),
     actions: {
         async getTestl() {
             try {
-                const res = await axiosInstance.get('/user/settings')
-                this.tests = res
+                const res = await axiosInstance.get<Test>('/user/settings')
+                this.test = res.data.personal
             } catch (e) {
                 console.log(e.response.data)
             }
