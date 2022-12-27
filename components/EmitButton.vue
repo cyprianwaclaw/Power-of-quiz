e55087646aab1d085b774e6170ef20ec8d38459a
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2022-12-27 22:39:33 +0100

    s

--- components/EmitButton.vue
+++ components/EmitButton.vue
@@ -1,43 +1,63 @@
+<template>
+  <label>{{ label }}</label>
+    <input
+    name="name"
+     class="base-input"
+     :inputvalue="`${props.inputValue}`"
+     :class="{'is-invalid': !!errors.length }"
+     :placeholder="`${props.placeholder}`"
+      v-model="value"
+      @input="$emit('update:modelValue', $event.target.value)"
+  
+    />  
+
+<span class="active">{{ errors[0] }}</span>
+<p>{{ value }}</p>
+<p>{{ meta.valid }}</p>
+</template>
+
 <script setup lang="ts">
 import {ref} from "vue"
+import * as yup from 'yup';
+import { useField } from 'vee-validate';
 import { useSettings } from "@/store/useSettings";
+
+const hasError = ref<boolean>(false)
+
+
+ const isActive = ref(true)
+const error = ref(null)
+
+// const classObject = computed(() => ({
+//   'is-invalid': errorMessage.value && !errorMessage.value,
+
+// }))
 
 const props = defineProps({
   modelValue: String,
   label: String,
   placeholder: String,
+  inputValue: String,
 })
 
-const value = ref('')
-
-const isActive = ref(true)
-
-const Message = computed(() => {
-  return value.value.length > 0 ? "Error" : "";
-});
-
-function error() {
-
-}
+const {value,  errors, meta } = useField<any>('name',
+yup.string().min(5, "Min is 5")
+   .max(22, "max is 20")
+// .value => !value
+ ); 
 
 const emit = defineEmits(['update:modelValue', 'submitTest'])
-
 </script>
 
-<template>
-  <label>{{ label }}</label>
-  <input
-   class="base-input"
-   :class="{ active: isActive }"
-   :placeholder="`${props.placeholder}`"
-    v-model="value"
-    @input="$emit('update:modelValue', $event.target.value)"
-
-  />
-{{ Message }}
-</template>
-<style>
-.active{
-  color: red;
+<style scoped>
+.is-invalid{
+  border-width: 1px;
+  border-color: red;
 }
+
+.active{
+  color:red;
+}
+
+
 </style>
